18df32c18734d01b21133a2bf8c3feefaf19e917
EC2 Default User <ec2-user@ip-172-31-10-10.us-east-2.compute.internal>
2021-09-12 22:49:39 +0000

    HW1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,32 +4,124 @@
 
 def sum arr
   # YOUR CODE HERE
+  if arr==[]
+    return 0
+  end
+  
+  sum=0
+  for i in 0...arr.length
+    sum+=arr[i]
+  end
+  return sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+# Empty array returns 0. 
+  if arr == []
+  	return 0
+  end
+  
+  # Array with only one element. 
+  if arr.length == 1
+    return arr[0]
+  end
+  # Implementing max_2_sum
+  if arr[0] > arr[1]
+    first = arr[0]
+    second = arr[1]
+  else
+    first = arr[1]
+    second = arr[0]
+  end
+  
+  for i in 2...arr.length
+    if arr[i] >= first
+      second = first
+      first = arr[i]
+    elsif arr[i] > second 
+      second = arr[i]
+    end
+  end
+  return (first + second)
+
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  if arr==[]
+    return false
+  elsif arr.length == 1
+    return false
+  else
+    for i in 0...arr.length-1
+      for j in (i+1)...arr.length
+        if arr[i]+arr[j]==n
+          return true
+        end
+      end
+    end
+  end  
+  return false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, #{name}";
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  return (s =~ /[bcdfghjklmnpqrstvwxyz]/i) == 0
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s==''
+    return false
+  else
+    for i in 0...s.length
+      if s[i] !='1' and s[i] !='0'
+        return false
+      end
+    end
+  end
+  return s.to_i(2) % 4==0
+  
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  def initialize(isbn,price)
+    if isbn.size==0 or price<=0
+      raise  ArgumentError
+    end
+    @isbn=isbn
+    @price=price
+  end
+  
+  def isbn
+    @isbn
+  end
+  
+  def price
+    @price
+  end
+  
+  def isbn=(new_isbn)
+    @isbn=new_isbn
+  end
+  
+  def price=(new_price)
+    @price=new_price
+  end
+  
+  def price_as_string
+    format("$%0.2f",@price)
+  end
+  
 end
